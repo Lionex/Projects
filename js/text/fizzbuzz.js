9fd1b35c656cece3a682351171bb8f1939f024be
Gwen Lofman <gwen@lofman.co>
2016-09-04 19:16:49 -0400

    Removes semicolons

--- js/text/fizzbuzz.js
+++ js/text/fizzbuzz.js
@@ -1,52 +1,54 @@
-(function ()
+function main ()
 {
 
 function range(start, step, end)
 {
-    var array = [];
-    for (start; start < end; start += step)
+    var array = []
+    for (start ; start < end ; start += step)
     {
-        array.push(start);
+        array.push(start)
     }
-    return array;
+    return array
 }
 
 function print(x)
 {
-    console.log(x);
+    console.log(x)
 }
 
 var isDivisible = function (n)
 {
     return function (x)
     {
-        return x % n === 0;
+        return x % n === 0
     }
 }
 
-var isFizz = isDivisible(3);
-var isBuzz = isDivisible(5);
-var isFizzBuzz = function (x) { return isFizz(x) && isBuzz(x) };
+var isFizz = isDivisible(3)
+var isBuzz = isDivisible(5)
+var isFizzBuzz = function (x) { return isFizz(x) && isBuzz(x) }
 
 var fizzBuzz = function (n) {
     if (isFizzBuzz(n))
     {
-        return "FizzBuzz";
+        return "FizzBuzz"
     }
     else if (isFizz(n))
     {
-        return "Fizz";
+        return "Fizz"
     }
     else if (isBuzz(n))
     {
-        return "Buzz";
+        return "Buzz"
     }
     else
     {
-        return String(n);
+        return String(n)
     }
 }
 
-range(1,1,100).map(fizzBuzz).forEach(print);
+range(1,1,100).map(fizzBuzz).forEach(print)
 
-})();
+}
+
+main()
