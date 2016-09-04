b63b6cc495614d5144a8c5b9b670de48172efe1a
Gwen Lofman <gwen@lofman.co>
2016-09-04 00:28:49 -0400

    Adds range helper function

--- js/text/fizzbuzz.js
+++ js/text/fizzbuzz.js
@@ -1,4 +1,14 @@
 (function ()
 {
-    
+
+function range(start, step, end)
+{
+    var array = [];
+    for (var n = 0; n < end - start; n += step)
+    {
+        array[n] = n + start;
+    }
+    return array;
+}
+
 })();
